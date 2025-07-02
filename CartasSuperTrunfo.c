f1dddd63af2f02ef391ef1550b0fe8382b174d72
fla20-maker <gabfla3000@gmail.com>
2025-07-02 22:15:37 +0000

    ta quase lá

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,36 +4,35 @@
 
 int main() {
     char estado;
-    char codigo[3];
-    char nomedacidade[30];
+    char codigo[3],nomedacidade[30];
     int populacao;
     float area;
     float pib;
     int pontosturisticos;
     
     printf("digite o estado: \n");
-    scanf("%c, &estado");
-    printf("atribua um codigo(lembrando que a letra no inicio do codigo deve ser a mesma do estado, e o numero e apenas de 01 a 04.): \n");
-    scanf("%s, &codigo");
+    scanf("%c", &estado);
+    printf("atribua um codigo: \n");
+    scanf("%s", &codigo);
     printf("digite a cidade: \n");
-    scanf("%s, &nomedacidade");
+    scanf("%s", &nomedacidade);
     printf("incira a populaçao: \n");
-    scanf("%d, &populacao");
+    scanf("%d", &populacao);
     printf("incira a area: \n");
-    scanf("%f, &area");
+    scanf("%f", &area);
     printf("incira o pib: \n");
-    scanf("%f, &pib");
+    scanf("%f", &pib);
     printf("incira seus pontos turisticos: \n");
-    scanf("%d, &pontosturisticos");
+    scanf("%d", &pontosturisticos);
 
     printf("carta 1: \n");
-    printf("estado: %s\n", estado);
-    printf("estado: %s\n", codigo);
-    printf("estado: %s\n", nomedacidade);
-    printf("estado: %d\n", populacao);
-    printf("estado: %f\n", area);
-    printf("estado: %f\n", pib);
-    printf("estado: %d\n", pontosturisticos);
+    printf("estado: %c \n", estado);
+    printf("codigo: %s \n", codigo);
+    printf("cidade nomeada: %s \n", nomedacidade);
+    printf("populaçao: %d habitantes \n", populacao);
+    printf("area: %f kilometros quadrados\n", area);
+    printf("produto interno bruto: %f bilhoes de reais \n", pib);
+    printf("quantidade de pontos turisticos: %d \n", pontosturisticos);
 
     return 0;
 }
