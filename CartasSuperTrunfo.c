1a6cfd2bb8fe6d4a7006556f23f6e05a8f68c1db
fla20-maker <gabfla3000@gmail.com>
2025-07-04 22:11:38 +0000

    estresse puro esta merda!!!!

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,12 +4,16 @@
 
 int main() {
     char estado;
+    char codigo[3];
+    char cidade[50];
     int populacao;
     float area;
     float pib;
     int pontosturisticos;
 
     char estado2;
+    char codigo2[3];
+    char cidade2[50];
     int populacao2;
     float area2;
     float pib2;
@@ -17,6 +21,10 @@ int main() {
 
     printf("digite o estado: \n");
     scanf("%c", &estado);
+    printf("incira um codigo: \n");
+    scanf("%s", codigo);
+    printf("incira a cidade: \n");
+    scanf("%s", cidade);
     printf("incira a populaçao: \n");
     scanf("%d", &populacao);
     printf("incira a area: \n");
@@ -26,15 +34,12 @@ int main() {
     printf("incira seus pontos turisticos: \n");
     scanf("%d", &pontosturisticos);
 
-    printf("carta 1: \n");
-    printf("estado: %c \n", estado);
-    printf("populaçao: %d habitantes \n", populacao);
-    printf("area: %f kilometros quadrados\n", area);
-    printf("produto interno bruto: %f bilhoes de reais \n", pib);
-    printf("quantidade de pontos turisticos: %d \n", pontosturisticos);
-    
     printf("digite o estado: \n");
     scanf("%c", &estado2);
+    printf("incira um codigo: \n");
+    scanf("%s", codigo2);
+    printf("incira a cidade: \n");
+    scanf("%s", cidade2);
     printf("incira a populaçao: \n");
     scanf("%d", &populacao2);
     printf("incira a area: \n");
@@ -44,8 +49,19 @@ int main() {
     printf("incira seus pontos turisticos: \n");
     scanf("%d", &pontosturisticos2);
 
+    printf("carta 1: \n");
+    printf("estado: %c \n", estado);
+    printf("codigo: %s \n", codigo);
+    printf("cidade: %s \n", cidade);
+    printf("populaçao: %d habitantes \n", populacao);
+    printf("area: %f kilometros quadrados\n", area);
+    printf("produto interno bruto: %f bilhoes de reais \n", pib);
+    printf("quantidade de pontos turisticos: %d \n", pontosturisticos);
+
     printf("carta 2: \n");
     printf("estado: %c \n", estado2);
+    printf("codigo: %s \n", codigo2);
+    printf("cidade: %s \n", cidade2);
     printf("populaçao: %d habitantes \n", populacao2);
     printf("area: %f kilometros quadrados\n", area2);
     printf("produto interno bruto: %f bilhoes de reais \n", pib2);
